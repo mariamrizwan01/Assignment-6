78b40f645e5acf52f4aa1ab49410daae289cf026
mariamrizwan01 <mariam.rizwan01@gmail.com>
2024-03-27 21:11:37 +0500

    List

---
+++ Question1.dart
@@ -0,0 +1,9 @@
+// Q.1: Create a list of names and print all names using list.
+
+void main() {
+  //using List for declearing name
+  List name = ["Mariam", "Faiqa", "Eman", "Areej", "Mubashira"];
+
+  //Print the List of name 
+  print(name);
+}
